dc5cd7621c0ea8125c56206e56adb3c78488d4d8
coofzilla <jeric614@yahoo.com>
2024-01-13 09:01:36 +0900

    created cardview component; includes creation of card; established state for isFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,15 +7,38 @@
 
 import SwiftUI
 
+// "behaves like a View" (or whatever protocol) Protocol Oriented Program
 struct ContentView: View {
+    // Computed Property the value of body is computed; what is inside the {}
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+
+        .foregroundColor(.orange)
+        .imageScale(.small)
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp = false
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        }
     }
 }
 
